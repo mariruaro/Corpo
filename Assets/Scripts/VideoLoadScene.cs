08ac256fbc3a3dfe1a230f0e03d48c833f1f1858
Mariana Viganó <mariana@fdd.com.br>
2018-09-11 14:11:26 -0300

    novos

---
+++ Assets/Scripts/VideoLoadScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VideoLoadScene : MonoBehaviour {
+
+
+    public float delay = 15;
+    public string NewLevel = "Ambient1";
+    void Start()
+    {
+        StartCoroutine(LoadLevelAfterDelay(delay));
+    }
+
+    IEnumerator LoadLevelAfterDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(NewLevel);
+    }
+}
